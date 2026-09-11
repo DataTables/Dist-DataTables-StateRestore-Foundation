58c560414c964ae72e124b054867996b65df264f
dtbuild <dtbuild@rigil>
2026-09-11 12:07:38 +0000

    13ee95dd46967bade46b507b4de58f84c0891af6 Release 2.0.0
    Sync to source repo @13ee95dd46967bade46b507b4de58f84c0891af6

--- js/stateRestore.foundation.mjs
+++ js/stateRestore.foundation.mjs
@@ -1,4 +1,4 @@
-/*! StateRestore Foundation styling 2.0.0-dev for DataTables
+/*! StateRestore Foundation styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
